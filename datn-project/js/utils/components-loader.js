ac5d4680b5e32e30bb526d5bfbf0a7df4314368b
MAP1605 <cuongdvth03230@fpt.edu.vn>
2025-04-01 01:12:28 +0700

    Hoàn thành banner

--- datn-project/js/utils/components-loader.js
+++ datn-project/js/utils/components-loader.js
@@ -2,3 +2,4 @@ import { loadHTML } from './dom.js';  // Import hàm loadHTML từ file kia
 
 loadHTML('#header', 'components/header.html');  // Gọi hàm, chèn header
 loadHTML('#footer', 'components/footer.html');  // Gọi hàm, chèn footer
+loadHTML('#banner', 'components/banner.html');
