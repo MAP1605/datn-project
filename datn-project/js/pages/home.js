54f6e2d2f168c79d3474cff6fe34e0dad1397bd4
MAP1605 <cuongdvth03230@fpt.edu.vn>
2025-04-01 04:12:36 +0700

    Hoàn thành danh mục

---
+++ datn-project/js/pages/home.js
@@ -0,0 +1,11 @@
+// const toggleBtn = document.querySelector('.category__toggle-btn');
+// const categoryList = document.querySelector('.category__list');
+
+// if (toggleBtn && categoryList) {
+//     toggleBtn.addEventListener('click', () => {
+//         categoryList.classList.toggle('show-more');
+//         toggleBtn.textContent = categoryList.classList.contains('show-more') 
+//             ? 'Ẩn bớt' 
+//             : 'Xem thêm';
+//     });
+// }
