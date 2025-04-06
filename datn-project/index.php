bb15b169171dd97ed5c94486356ad8ff29b614d4
dainguyen6969 <daintth03468@fpt.edu.vn>
2025-04-07 02:16:42 +0700

    Giải quyết xung đột của các đường link

--- datn-project/index.php
+++ datn-project/index.php
@@ -6,25 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PearNK - Trang chủ</title>
     <!-- CSS (main) -->
-<<<<<<< HEAD:datn-project/index.html
-    <link rel="stylesheet" href="/datn-project/css/main.css">
-=======
-    <link rel="stylesheet" href="css/main.css">
->>>>>>> main:datn-project/index.php
+    <link rel="stylesheet" href="/datn-project/datn-project/css/main.css">
     <!-- CSS (font) -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <!-- CSS (icon) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-<<<<<<< HEAD:datn-project/index.html
     <!-- logo ở tên miền -->
-    <link rel="icon" type="image/png" href="../assets/images/logo/CuongDao__Logo-PEARNK.png" sizes="16x16">
-=======
-    <!-- JS: load component (header/banner/category/product/footer) -->
-    <script type="module" src="js/utils/components-loader.js"></script>
-
->>>>>>> main:datn-project/index.php
+    <link rel="icon" type="image/png" href="/datn-project/assets/images/logo/CuongDao__Logo-PEARNK.png" sizes="16x16">
 
 </head>
 
@@ -47,7 +37,6 @@
         <!-- End category (danh mục) -->
 
         <!-- Start prodcut -->
-        <!-- index.php -->
         <div id="products">
             <?php include 'product.php'; ?>
         </div>
@@ -55,17 +44,13 @@
     </main>
 
     <!-- Start footer -->
-    <div id="footer">
-    </div>
+    <footer id="footer">
+    </footer>
     <!-- End footer -->
 
-<<<<<<< HEAD:datn-project/index.html
     <!-- JS: load component (header/banner/category/product/footer) -->
-    <script type="module" src="/datn-project/js/utils/components-loader.js"></script>
-    <script type="module" src="/datn-project/js/pages/product.js"></script>
-=======
-    <script type="module" src="js/pages/home.js"></script>
->>>>>>> main:datn-project/index.php
+    <script type="module" src="/datn-project/datn-project/js/utils/components-loader.js"></script>
+    <script type="module" src="/datn-project/datn-project/js/pages/product.js"></script>
 </body>
 
 </html>
--- datn-project/js/pages/product.js
+++ datn-project/js/pages/product.js
@@ -44,17 +44,6 @@ function setupShowMoreButton() {
 
   if (showMoreBtn) {
     showMoreBtn.addEventListener("click", () => {
-<<<<<<< HEAD:datn-project/js/pages/product.js
-      console.log("👉 Nút 'Xem thêm' đã được bấm!");
-
-      // Hiện tất cả item
-      hiddenItems.forEach(item => {
-        item.style.display = "block";
-      });
-
-      // Ẩn nút sau khi bấm
-      showMoreBtn.style.display = "none";
-=======
       // cập nhật lại danh sách ID đã hiển thị
       document.querySelectorAll(".product__item").forEach(item => {
         const id = item.getAttribute("data-id");
@@ -80,24 +69,16 @@ function setupShowMoreButton() {
           showMoreBtn.style.display = "none"; // ✅ ẩn nút sau khi bấm (luôn)
         })
         .catch(err => console.error("❌ Lỗi khi tải thêm sản phẩm:", err));
->>>>>>> main:datn-project/js/pages/home.js
     });
   }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-<<<<<<< HEAD:datn-project/js/pages/product.js
-  fetch("components/product.html")
-    .then(res => res.text())
-    .then(html => {
-      document.querySelector("#product").innerHTML = html;
-      setupShowMoreButton(); // gọi sau khi load xong sản phẩm
-=======
   fetch("components/product.php")
     .then(res => res.text())
     .then(html => {
       document.querySelector("#products").innerHTML = html;
       setupShowMoreButton();
->>>>>>> main:datn-project/js/pages/home.js
     });
 });
+
