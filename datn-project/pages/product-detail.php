a7c7af6ac9f395c1355b335ebd0b4fee81c57059
dainguyen6969 <daintth03468@fpt.edu.vn>
2025-04-07 18:46:11 +0700

    Lỗi giao diện đánh giá sản phẩm

--- datn-project/pages/product-detail.php
+++ datn-project/pages/product-detail.php
@@ -368,23 +368,24 @@ if ($resultAvg && $rowAvg = $resultAvg->fetch_assoc()) {
                                 </div>
                             <?php endwhile; ?>
                         </div>
+                    </div>
 
-                        <!-- Phân trang đánh giá -->
-                        <div class="review__pagination" id="review__pagination">
-                            <button class="page-btn"><i class="fa-solid fa-angle-left"></i></button>
-                            <button class="page-btn active">1</button>
-                            <button class="page-btn">2</button>
-                            <button class="page-btn">3</button>
-                            <button class="page-btn">4</button>
-                            <button class="page-btn">5</button>
-                            <button class="page-btn">...</button>
-                            <button class="page-btn">6</button>
-                            <button class="page-btn">7</button>
-                            <button class="page-btn">8</button>
-                            <button class="page-btn"><i class="fa-solid fa-angle-right"></i></button>
-                        </div>
+                    <!-- Phân trang đánh giá -->
+                    <div class="review__pagination" id="review__pagination">
+                        <button class="page-btn"><i class="fa-solid fa-angle-left"></i></button>
+                        <button class="page-btn active">1</button>
+                        <button class="page-btn">2</button>
+                        <button class="page-btn">3</button>
+                        <button class="page-btn">4</button>
+                        <button class="page-btn">5</button>
+                        <button class="page-btn">...</button>
+                        <button class="page-btn">6</button>
+                        <button class="page-btn">7</button>
+                        <button class="page-btn">8</button>
+                        <button class="page-btn"><i class="fa-solid fa-angle-right"></i></button>
                     </div>
                 </div>
+            </div>
         </section>
         <!-- End product-detail -->
 
