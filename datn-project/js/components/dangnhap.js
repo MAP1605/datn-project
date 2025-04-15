313856caf5f30fee983febf6c58566aff57e890c
dungthephendasdasdsadas <dungptth03483@fpt.edu.vn>
2025-04-15 22:27:53 +0700

    cập nhật đăng nhập hiện avata

--- datn-project/js/components/dangnhap.js
+++ datn-project/js/components/dangnhap.js
@@ -69,3 +69,27 @@ function showNotification(message, type) {
     noti.remove();
   }, 3000);
 }
+    
+
+document.addEventListener("DOMContentLoaded", function () {
+  const loginBtn = document.querySelector(".login__button");
+
+  if (loginBtn) {
+    loginBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+
+   
+
+      // 1. Ẩn các nút đăng nhập/đăng ký
+      const authLinks = document.querySelectorAll("#headerAuth > a");
+      authLinks.forEach(link => link.style.display = "none");
+
+      // 2. Hiện avatar
+      const userSection = document.querySelector(".header__user");
+      if (userSection) userSection.style.display = "inline-flex";
+
+      // 3. Chuyển trang (tuỳ chọn)
+
+    });
+  }
+});
